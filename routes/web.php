c82e7c4ae9eae0165e71105336e2b1ea2484d12f
angelBosco <avela@iesjuanbosco.es>
2026-05-21 20:56:29 +0200

    Equipo de Juan en rama Juan

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/hola', function () {
     return "<h1>Hola Mundo</h1>";
 });
+
+Route::get('/equipo', function () {
+    return "<h1>El equipo de Juan es el Madrid</h1>";
+});
